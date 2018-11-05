a16f6e680675c1800d73b721d489ee5488e59fe3
ShacharHarshuv <shachar.harshuv@gmail.com>
2018-11-05 20:26:50 +0200

    Initialize server

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
-var exec = require('child_process').execFile;
+const express = require('express');
+const app = express();
+const exec = require('child_process').execFile;
 
-exec('exec/HelloWorld.exe', function(err, data) {  
-    console.log(err)
-    console.log(data.toString());                       
-});  
+app.get('/', function(req, res){
+    exec('exec/HelloWorld.exe', function(err, data) {  
+        if (err){
+            res.write("ERROR: ", err);
+        }
+        else{
+            res.write(data);
+        }
+        res.end();                      
+    }); 
+})
+
+app.listen(process.env.PORT || 1234, function(){
+    console.log("Listening..."); 
+});
